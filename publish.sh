11a637386bfadd583d81430da0b0a3952c015b02
Tim Broder <timothy.broder@gmail.com>
2015-07-12 12:09:51 -0400

    travis updates

--- publish.sh
+++ publish.sh
@@ -15,3 +15,5 @@ git commit -m "Build website"
 git filter-branch --subdirectory-filter output_prod/ -f
 
 git push "https://github.com/broderboy/timbroder.com-sculpin" -f gh-pages
+
+if [ $? -ne 0 ]; then echo "Could not push the site"; exit 1; fi
