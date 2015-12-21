57b4035e01ddc4e1798746ff77cceeef570cce3c
Tim Broder <timothy.broder@gmail.com>
2015-12-20 19:58:52 -0500

    Backend of Algolia search

--- app/SculpinKernel.php
+++ app/SculpinKernel.php
@@ -7,7 +7,8 @@ class SculpinKernel extends \Sculpin\Bundle\SculpinBundle\HttpKernel\AbstractKer
         return array(
             'Jb\Bundle\TagCloudBundle\JbTagCloudBundle',
             'Mavimo\Sculpin\Bundle\RedirectBundle\SculpinRedirectBundle',
-            'Mavimo\Sculpin\Bundle\EditorBundle\SculpinEditorBundle'
+            'Mavimo\Sculpin\Bundle\EditorBundle\SculpinEditorBundle',
+            'Timbroder\Bundle\AlgoliaBundle\TimbroderAlgoliaBundle'
         );
     }
 }
