abb609b5de2d8284426232bbc89a7a03d0b723c0
Tim Broder <timothy.broder@gmail.com>
2023-02-14 14:04:44 -0500

    WIP posts

--- src/components/posts/posts.js
+++ src/components/posts/posts.js
@@ -1,12 +1,12 @@
 import * as React from "react"
-import Post from "./post";
+import PostCard from "./postCard";
 
 export default function Posts({posts: posts}) {
     return (
         <div className="md:border-l md:border-zinc-100 md:pl-6">
             <div className="flex max-w-3xl flex-col space-y-16">
                 {posts.map((post) => (
-                    <Post key={post.fields.slug} post={post}/>
+                    <PostCard key={post.fields.slug} post={post}/>
                 ))}
             </div>
         </div>
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout/layout"
 import Seo from "../components/seo"
+import {Prose} from "../components/content/pros";
 
 const BlogPostTemplate = ({
   data: { previous, next, site, markdownRemark: post },
@@ -14,22 +15,39 @@ const BlogPostTemplate = ({
   return (
     <Layout location={location} title={siteTitle}>
       <article
-        className="blog-post"
         itemScope
         itemType="http://schema.org/Article"
       >
-        <header>
-          <h1 itemProp="headline">{post.frontmatter.title}</h1>
-          <p>{post.frontmatter.date}</p>
-        </header>
-        <section
-          dangerouslySetInnerHTML={{ __html: post.html }}
-          itemProp="articleBody"
-        />
-        <hr />
-        <footer>
-          <Bio />
-        </footer>
+          <header className="flex flex-col">
+              <h1 className="mt-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+                  {post.frontmatter.title}
+              </h1>
+              <time
+                  dateTime={post.frontmatter.date}
+                  className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
+              >
+                  <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
+                  <span className="ml-3">{post.frontmatter.date}</span>
+              </time>
+          </header>
+          <Prose className="mt-8">
+              <section
+                dangerouslySetInnerHTML={{ __html: post.html }}
+                itemProp="articleBody"
+              />
+          </Prose>
+        {/*<header>*/}
+        {/*  <h1 itemProp="headline">{post.frontmatter.title}</h1>*/}
+        {/*  <p>{post.frontmatter.date}</p>*/}
+        {/*</header>*/}
+        {/*<section*/}
+        {/*  dangerouslySetInnerHTML={{ __html: post.html }}*/}
+        {/*  itemProp="articleBody"*/}
+        {/*/>*/}
+        {/*<hr />*/}
+        {/*<footer>*/}
+        {/*  <Bio />*/}
+        {/*</footer>*/}
       </article>
       <nav className="blog-post-nav">
         <ul
