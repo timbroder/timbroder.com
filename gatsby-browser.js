c4faea08cbfc86604c3af26d100bdfa17e13e63f
Tim Broder <timothy.broder@gmail.com>
2023-02-14 15:37:07 -0500

    Update gatsby-config and global.css to add line-numbers and okaidia theme to prismjs, and update styling of code blocks.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,7 +7,9 @@
 // import "./src/style.css"
 
 // Highlighting for code blocks
-import "prismjs/themes/prism.css"
+import "prismjs/themes/prism-okaidia.css"
 
 // Tailwind
 import './src/styles/global.css'
+
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,12 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          `gatsby-remark-prismjs`,
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              showLineNumbers: true
+            }
+          },
         ],
       },
     },
