72fc5e471c498dcd42d80f35ae410152ee44a8c4
Tim Broder <timothy.broder@gmail.com>
2023-03-10 16:17:11 -0500

    center nav

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -32,7 +32,7 @@ export default function Header({location}) {
 
     return (
         <Fragment>
-            <div className="top-0 z-10 h-16 pt-6 flex flex-1 md:justify-center">
+            <div className="top-0 z-10 h-16 pt-6 flex flex-1 justify-center">
                 <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur">
                     {navItems.map((item) => (
                         <NavItem
